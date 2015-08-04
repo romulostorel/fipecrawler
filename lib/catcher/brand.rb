8656ce772310b4b41842b48746579352c211d11f
Romulo Storel <romulostorel@gmail.com>
2015-08-04 20:05:34 -0300

    Refactoring BrandCatcher

--- lib/catcher/brand.rb
+++ lib/catcher/brand.rb
@@ -1,10 +1,25 @@
 require 'net/http'
 
-module Catcher
-  class Brand
-    def self.catch
-      uri = URI('http://www.fipe.org.br/IndicesConsulta-ConsultarMarcas')
-      Net::HTTP.post_form(uri, 'codigoTabelaReferencia' => 182, 'codigoTipoVeiculo' => 1).body.force_encoding("UTF-8")
-    end
+class Catcher::Brand
+  def self.catch
+    new.catch
+  end
+
+  def catch
+    get_json
+  end
+
+  protected
+
+  def get_json
+    post(params: {'codigoTabelaReferencia' => 182, 'codigoTipoVeiculo' => 1 } ).body.force_encoding("UTF-8")
+  end
+
+  def post(params:)
+    Net::HTTP.post_form(uri, params)
+  end
+
+  def uri
+    URI('http://www.fipe.org.br/IndicesConsulta-ConsultarMarcas')
   end
 end
