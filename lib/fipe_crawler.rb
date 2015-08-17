f3cb3987656c7b89019579251a9992cb07826738
Romulo Storel <romulostorel@gmail.com>
2015-08-17 16:19:43 -0300

    Adding parse to fipe_crawler requires

--- lib/fipe_crawler.rb
+++ lib/fipe_crawler.rb
@@ -1,8 +1,9 @@
 require 'data_mapper'
+require './lib/db_connector'
 require './lib/models'
 require './lib/catcher'
 require './lib/importer'
-require './lib/db_connector'
+require './lib/parser'
 
 class FipeCrawler
 end
