3b5baf5ca99a0654537d8ba6025c75d13ef9544a
Romulo Storel <romulostorel@gmail.com>
2015-09-15 16:33:10 -0300

    Refactoring models
    * Moving columns definitions to inside the class

--- lib/models.rb
+++ lib/models.rb
@@ -1,13 +1,13 @@
 module Models
   class Brand < Sequel::Model
+    set_allowed_columns :id, :name
+
+    one_to_many :vehicles
   end
 
   class Vehicle < Sequel::Model
-    def brand
-      Brand[brand_id]
-    end
+    set_allowed_columns :id, :name, :brand_id
+
+    many_to_one :brand
   end
 end
-
-Models::Brand.set_allowed_columns :id, :name
-Models::Vehicle.set_allowed_columns :id, :name, :brand_id
