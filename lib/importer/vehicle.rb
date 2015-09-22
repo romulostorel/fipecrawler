5cf78c06eab5a69a25332aee488a5b8cf66a7e2c
Romulo Storel <romulostorel@gmail.com>
2015-09-22 18:13:53 -0300

    Refactoring VehicleImporter
    * Removing accessors

--- lib/importer/vehicle.rb
+++ lib/importer/vehicle.rb
@@ -1,15 +1,12 @@
 require 'catcher/vehicle'
 
 class Importer::Vehicle
-  attr_accessor :catcher, :model, :parser
-
   def initialize(type:, reference:)
     @type = type
     @reference = reference
-
-    self.catcher = Catcher::Vehicle
-    self.model = Models::Vehicle
-    self.parser = Parser::Vehicle
+    @catcher = Catcher::Vehicle
+    @model = Models::Vehicle
+    @parser = Parser::Vehicle
   end
 
   def self.import
@@ -25,12 +22,12 @@ class Importer::Vehicle
   def create_vehicles
     Models::Brand.all.each do |brand|
       catch_and_parse_vehicles(brand.id, @type, @reference).each do |vehicle|
-        model.create(vehicle.merge({ type: @type }))
+        @model.create(vehicle.merge({ type: @type }))
       end
     end
   end
 
   def catch_and_parse_vehicles(brand_id, type, reference)
-    parser.parse(catcher.catch(brand_id: brand_id, type: type, reference: reference))
+    @parser.parse(@catcher.catch(brand_id: brand_id, type: type, reference: reference))
   end
 end
